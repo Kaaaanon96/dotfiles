1a2a560b784b26bd3ec4d9029db11cf184496882
Kaaaanon96 <19369951+Kanon159@users.noreply.github.com>
2026-01-16 19:20:31 +0900

    Update: wezterm conf

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -2,21 +2,86 @@ local wezterm = require("wezterm")
 local config = wezterm.config_builder()
 
 local home_dir =  wezterm.home_dir
+local SOLID_LEFT_ARROW = wezterm.nerdfonts.ple_lower_right_triangle
+local SOLID_RIGHT_ARROW = wezterm.nerdfonts.ple_upper_left_triangle
+local LIST_ORDER = wezterm.nerdfonts.cod_list_ordered
+local SPLIT_ICON = wezterm.nerdfonts.cod_chevron_right
+local RIGHT_ARROW = wezterm.nerdfonts.oct_triangle_right
+local WORKSPACE_ICON = wezterm.nerdfonts.md_folder_open_outline
+local EDIT_ICON = wezterm.nerdfonts.md_folder_edit_outline
 
+--------------------------
 -- hot reload
+--------------------------
 config.automatically_reload_config = true
 
+--------------------------
+-- base
+--------------------------
 config.scrollback_lines = 3500
 
+--------------------------
 -- color scheme
+--------------------------
+config.color_scheme = "Dracula (Official)"
 -- config.color_scheme = "Dracula"
 -- config.color_scheme = "Dracula (base16)"
 -- config.color_scheme = "Dracula (Official)"
 -- config.color_scheme = "Dracula (Gogh)"
 -- config.color_scheme = "Dracula+"
-config.color_scheme = "Dracula (Official)"
 
+--------------------------
+-- window/tab/pane design
+--------------------------
+config.inactive_pane_hsb = {
+  saturation = 0.6,
+  brightness = 0.1,
+}
+
+config.use_fancy_tab_bar = false
+config.show_new_tab_button_in_tab_bar = false
+config.tab_max_width = 50
+
+local schemes = wezterm.color.get_builtin_schemes()
+local scheme = schemes['Catppuccin Latte']
+config.colors = { tab_bar = scheme.tab_bar }
+config.colors.tab_bar.active_tab.intensity = "Bold"
+wezterm.on("format-tab-title", function(tab, tabs, panes, config, hover, max_width)
+  local index = tab.tab_index + 1
+  local truncate_limit = math.max(1, max_width - 10)
+  local truncate_title = wezterm.truncate_right(tab.active_pane.title, truncate_limit)
+  local full_title = " " .. index .. ": " .. truncate_title .. " "
+
+  local bg
+  local fg
+  if tab.is_active then
+    bg = scheme.tab_bar.active_tab.bg_color
+    fg = scheme.tab_bar.active_tab.fg_color
+  elseif hover then
+    bg = scheme.tab_bar.inactive_tab_hover.bg_color
+    fg = scheme.tab_bar.inactive_tab_hover.fg_color
+  else
+    bg = scheme.tab_bar.inactive_tab.bg_color
+    fg = scheme.tab_bar.inactive_tab.fg_color
+  end
+  local edge_background = scheme.tab_bar.background
+
+  return {
+    { Background = { Color = edge_background } },
+    { Foreground = { Color = bg } },
+    { Text = SOLID_LEFT_ARROW },
+    { Background = { Color = bg } },
+    { Foreground = { Color = fg } },
+    { Text = full_title },
+    { Background = { Color = edge_background } },
+    { Foreground = { Color = bg } },
+    { Text = SOLID_RIGHT_ARROW },
+  }
+end)
+
+--------------------------
 -- bg
+--------------------------
 local bg_image_path = home_dir .. "/dotfiles/bg_img/092.jpg"
 config.background = {
   {
@@ -39,7 +104,9 @@ config.background = {
 }
 config.macos_window_background_blur = 10
 
+--------------------------
 -- font
+--------------------------
 config.font = wezterm.font("UDEV Gothic 35JPDOC")
 -- config.font = wezterm.font_with_fallback {
 --   { family = "UDEV Gothic 35JPDOC" },
@@ -49,15 +116,11 @@ config.font_size = 12.0
 config.freetype_load_target = "Light"
 config.front_end = "WebGpu"
 
--- window/tab/pane design
-config.show_new_tab_button_in_tab_bar = false
+config.use_ime = true
 
-config.inactive_pane_hsb = {
-  saturation = 0.6,
-  brightness = 0.1,
-}
-
+--------------------------
 -- mapping
+--------------------------
 config.disable_default_key_bindings = true
 
 config.leader = { key = "q", mods = "CTRL", timeout_milliseconds = 2000 }
@@ -67,8 +130,13 @@ config.keys = {
   -- copy/paste
   { key = "c", mods = "CMD", action = act.CopyTo("Clipboard") },
   { key = "v", mods = "CMD", action = act.PasteFrom("Clipboard") },
+  { key = "v", mods ="LEADER", action = act.ActivateCopyMode, },
   -- search && scroll
-  { key = "f", mods = "CMD", action = act.Search("CurrentSelectionOrEmptyString") },
+  {
+    key = "f",
+    mods = "CMD",
+    action = act.Search("CurrentSelectionOrEmptyString"),
+  },
   { key = "k", mods = "CMD", action = act.ClearScrollback("ScrollbackOnly") },
   -- pane
   { key = "d", mods = "CMD", action = act.SplitHorizontal({}) },
@@ -82,11 +150,63 @@ config.keys = {
   { key = "t", mods = "SHIFT|CMD", action = act.SpawnTab("CurrentPaneDomain") },
   { key = "[", mods = "CMD|SHIFT", action = act.ActivateTabRelative(-1) },
   { key = "]", mods = "CMD|SHIFT", action = act.ActivateTabRelative(1) },
-  { key = "w", mods = "LEADER", action = wezterm.action.CloseCurrentPane { confirm = true }, },
+  { key = "w", mods = "LEADER", action = act.CloseCurrentPane { confirm = true }, },
+  -- workspace
+  {
+    key = "s",
+    mods = "LEADER",
+    -- action = act.ShowLauncherArgs({ flags = "WORKSPACES", title = "Select workspace" }) 
+    action = wezterm.action_callback(function(win, pane)
+      local workspaces = {}
+      for i, name in ipairs(wezterm.mux.get_workspace_names()) do
+        table.insert(workspaces, {
+          id = name,
+          label = string.format("%d. %s", i, name)
+        })
+        local current = wezterm.mux.get_active_workspace()
+        win:perform_action(act.InputSelector({
+          action = wezterm.action_callback(function(_, _, id, label)
+            if not id and not label then
+              wezterm.log_info("Workspace selection canceled")
+            else
+              win:perform_action(act.SwitchToWorkspace({ name = id }), pane)
+            end
+          end),
+          title = "Select workspace",
+          choices = workspaces,
+          fuzzy = true,
+          fuzzy_description = string.format(" [wezterm] %s Select Workspace%s Current: %s%s Next: ", LIST_ORDER, SPLIT_ICON, current, RIGHT_ARROW),
+        }), pane)
+      end
+    end)
+  },
+  {
+    key = "S",
+    mods = "LEADER|SHIFT",
+    action = act.PromptInputLine({
+      description = string.format(" [wezterm] %s Create Workspace%s Enter Name:", WORKSPACE_ICON, SPLIT_ICON),
+      action = wezterm.action_callback(function(win, pane, line)
+        if line then
+          win:perform_action(act.SwitchToWorkspace({ name = line }), pane)
+        end
+      end),
+    })
+  },
+  {
+    key = "r",
+    mods = "LEADER",
+    action = act.PromptInputLine({
+      description = string.format(" [wezterm] %s Rename Workspace%s Enter New Name: ", EDIT_ICON, SPLIT_ICON),
+      action = wezterm.action_callback(function(win, pane, line)
+        if line then
+          wezterm.mux.rename_workspace(wezterm.mux.get_active_workspace(), line)
+        end
+      end),
+    })
+  },
   -- utils
   { key = "p", mods = "LEADER", action = act.ActivateCommandPalette },
 }
-
 -- mapping: tab select(1~9)
 for i = 1, 9 do
   table.insert(config.keys, {
@@ -95,5 +215,18 @@ for i = 1, 9 do
     action = act.ActivateTab(i - 1),
   })
 end
+config.mouse_bindings = {
+  {
+    event = { Up = { streak = 1, button = "Left" } },
+    mods = "CMD",
+    action = act.OpenLinkAtMouseCursor,
+  },
+}
+config.key_tables = {
+  search_mode = {
+    { key = "Enter", mods = "NONE", action = act.CopyMode("NextMatch") },
+    { key = "Enter", mods = "SHIFT",  action = act.CopyMode("PriorMatch") },
+  }
+}
 
 return config
